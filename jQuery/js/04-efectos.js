915928d9ee67e1ea289519bb424d59419ca68819
MirkoGarin <mirkogarin@gmail.com>
2019-09-16 17:17:32 -0300

    Terminado Clase 102

--- jQuery/js/04-efectos.js
+++ jQuery/js/04-efectos.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
-    
+$("#mostrar").hide();
 $("#mostrar").click(function(){
     $(this).hide();
     $("#ocultar").show();
-    $("#caja").show(); 
+    $("#caja").fadeTo('fast', 1); 
     })
                
 $("#ocultar").click(function(){
     $(this).hide();
     $("#mostrar").show();
-    $("#caja").hide(); 
+    $("#caja").fadeTo('slow', 0.2); 
     })
+$("#todoenuno").click(function(){
+    $("#caja").slideToggle('Fast');
+})
 });
