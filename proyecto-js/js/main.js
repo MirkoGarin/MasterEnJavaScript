6f495c194d7b64151af7480f8bf743378531c67f
MirkoGarin <mirkogarin@gmail.com>
2019-11-07 08:37:59 -0300

    133. Cambiador de temas del sitio web

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -42,7 +42,15 @@ var posts =[{
     `;
     $("#posts").append(post);
     });
-        
-
-
+//selector de temas  
+        var theme = $("#theme");
+      $("#to-green").click(function(){
+        theme.attr("href", "css/green.css");
+      });
+      $("#to-red").click(function(){
+        theme.attr("href", "css/red.css");
+      });
+      $("#to-blue").click(function(){
+        theme.attr("href", "css/blue.css");
+      });
 });
